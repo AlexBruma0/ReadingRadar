9c72034a48d87555a4d89957105731288890f58c
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-06-23 13:54:36 -0700

    deletions

--- components/Card/CardDetails/CardDetails.jsx
+++ components/Card/CardDetails/CardDetails.jsx
@@ -1,22 +1,9 @@
 import React, { useState, useEffect } from "react";
 import {
-  Calendar,
-  Check,
-  CheckSquare,
-  Clock,
-  CreditCard,
-  List,
-  Plus,
-  Tag,
   Trash,
-  Type,
-  X,
 } from "react-feather";
-import Editable from "../../Editable/Editable";
 import Modal from "../../Modal/Modal";
 import "./CardDetails.css";
-import { v4 as uuidv4 } from "uuid";
-import Label from "../../Label/Label";
 
 export default function CardDetails(props) {
   const colors = ["#61bd4f", "#f2d600", "#ff9f1a", "#eb5a46", "#c377e0"];
@@ -24,7 +11,6 @@ export default function CardDetails(props) {
   const [values, setValues] = useState({ ...props.card });
   const [input, setInput] = useState(false);
   const [text, setText] = useState(values.title);
-  const [labelShow, setLabelShow] = useState(false);
   const Input = (props) => {
     return (
       <div className="input">
@@ -38,32 +24,6 @@ export default function CardDetails(props) {
         />
       </div>
     );
-  };
-  const addTask = (value) => {
-    values.task.push({
-      id: uuidv4(),
-      task: value,
-      completed: false,
-    });
-    setValues({ ...values });
-  };
-
-  const removeTask = (id) => {
-    const remaningTask = values.task.filter((item) => item.id !== id);
-    setValues({ ...values, task: remaningTask });
-  };
-
-  const deleteAllTask = () => {
-    setValues({
-      ...values,
-      task: [],
-    });
-  };
-
-  const updateTask = (id) => {
-    const taskIndex = values.task.findIndex((item) => item.id === id);
-    values.task[taskIndex].completed = !values.task[taskIndex].completed;
-    setValues({ ...values });
   };
   const updateTitle = (value) => {
     setValues({ ...values, title: value });
