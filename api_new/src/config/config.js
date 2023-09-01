e564e9f076b82f36efa2e7eb85cb8d60ee6c7c6c
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-09-01 11:08:12 -0700

    bug

--- api_new/src/config/config.js
+++ api_new/src/config/config.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.database_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+//
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Connection error:'));
