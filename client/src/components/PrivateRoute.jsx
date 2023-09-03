55e3ab657ef9b116443dd207772349461be928ad
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-09-03 10:21:45 -0700

    private route

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+// PrivateRoute.js
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useSelector((state) => state.auth.login.isAuthenticated);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" /> // Redirect to login if not authenticated
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
