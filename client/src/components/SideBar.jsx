56b857e8eed1412215ddd42682e424131670c4a7
AlexBruma0 <brumaa222@gmail.com>
2024-01-05 11:38:29 -0800

    Add icons to sidebar links

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../components/ThemeContext";
 import { themes } from "../themes";
 import tinycolor from "tinycolor2";
+import { FaBook, FaUserFriends, FaCog, FaSignOutAlt } from 'react-icons/fa';
 
 const Sidebar = ({ isOpen, isFullWidth }) => {
   const dispatch = useDispatch();
@@ -23,39 +24,40 @@ const Sidebar = ({ isOpen, isFullWidth }) => {
     localStorage.setItem("viewingId", userId);
   };
 
+
   const links = [
-    { name: "My Library", icon: "📚", link: "/home" },
-    { name: "Friends", icon: "👥", link: "/users" },
-    { name: "Settings", icon: "⛭", link: "/settings" },
-    { name: "Logout", icon: "🫡", link: "/logout" },
+    { name: "My Library", icon: <FaBook  />, link: "/home" },
+    { name: "Friends", icon: <FaUserFriends  />, link: "/users" },
+    { name: "Settings", icon: <FaCog  />, link: "/settings" },
+    { name: "Logout", icon: <FaSignOutAlt  />, link: "/logout" },
   ];
 
-  return (
-    <div
-      style={{ backgroundColor: currentThemeColors.primary }}
-      className={`fixed left-0 h-full z-2 ${isFullWidth ? "w-full" : "w-64"} transform ${
-        isOpen ? "translate-x-0" : "-translate-x-full"
-      } transition-transform duration-300 ease-in-out`}
-    >
-      {links.map((link, index) => (
-        <Link
-          id="hov"
-          key={index}
-          className="block px-4 py-2"
-          to={link.link}
-          style={{
-            color: currentThemeColors.text,
-            "--hover-background": tinycolor(currentThemeColors.primary)
-              .darken(10)
-              .toString(),
-          }}
-        >
-          <span className="">{link.icon}</span>
-          <span className="ml-2 text-lg font-medium">{link.name}</span>
-        </Link>
-      ))}
-    </div>
-  );
-};
+    return (
+      <div
+        style={{ backgroundColor: currentThemeColors.primary }}
+        className={`fixed left-0 h-full z-2 ${isFullWidth ? "w-full" : "w-64"} transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out`}
+      >
+        {links.map((link, index) => (
+          <Link
+            id="hov"
+            key={index}
+            className="px-4 py-2 flex items-center" // Added items-center here
+            to={link.link}
+            style={{
+              color: currentThemeColors.text,
+              "--hover-background": tinycolor(currentThemeColors.primary)
+                .darken(10)
+                .toString(),
+            }}
+          >
+            <span className="">{link.icon}</span>
+            <span className="ml-2 text-lg font-medium">{link.name}</span>
+          </Link>
+        ))}
+      </div>
+    );
+  };
 
 export default Sidebar;
