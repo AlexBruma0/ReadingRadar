fe4ddc07b8622280f446d18d3b6c5904e19b289b
AlexBruma0 <brumaa222@gmail.com>
2024-01-05 10:34:51 -0800

    Add search icons and update sidebar icon in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,21 @@ import { Search } from "react-feather";
 import Image from "../../resources/logo.png";
 import { ThemeContext } from "../components/ThemeContext";
 import { themes } from "../themes";
+import { FaSearch } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
+import { MdSearch } from 'react-icons/md';
+import { FiSearch } from 'react-icons/fi';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FiMenu } from 'react-icons/fi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoIosMenu } from 'react-icons/io';
+import { MdMenu } from 'react-icons/md';
+import { FaBars } from 'react-icons/fa';
+
+
+// Use in your component
+
+import tinycolor from "tinycolor2";
 
 export default function Navbar({ toggleSidebar }) {
   const [searching, setSearching] = useState(false);
@@ -23,15 +38,28 @@ export default function Navbar({ toggleSidebar }) {
       className={`fixed top-0 w-full z-10 flex items-center p-4 `}
     >
       <div className="flex items-center">
-        <span className="text-xl mr-3 cursor-pointer" onClick={toggleSidebar}>
-          ☰
+        <span
+          id="hov"
+          className="text-xl mr-3 cursor-pointer p-4 rounded-full"
+          onClick={toggleSidebar}
+          style={{ 
+            "--hover-background": tinycolor(currentThemeColors.accent)
+            .darken(5)
+            .toString(),
+          }}
+        >
+          <FiMenu onClick={toggleSidebar} />
         </span>
-        <img src={Image} className="h-10 w-10 rounded-full" alt="Logo" />
+        <div className="cursor-pointer">
+          <Link to="/home">
+          <img src={Image} className="h-10 w-10 rounded-full" alt="Logo" />
+          </Link>
+        </div>
       </div>
       <div className="flex-grow"></div>
       <div>
         <button type="submit" className={`p-2 rounded-full `}>
-          🔍
+          <IoIosSearch size={25} />
         </button>
       </div>
     </nav>
