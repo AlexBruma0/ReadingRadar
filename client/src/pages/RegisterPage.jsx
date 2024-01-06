5ad590745f34946eaad09c0a4f3dacd9cddc29cf
AlexBruma0 <brumaa222@gmail.com>
2024-01-06 10:38:25 -0800

    Add loading spinner to registration button

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Image from "../../resources/logo.png";
 import { themes } from "../themes";
+import { SpinnerCircular } from "spinners-react";
 
 const RegistrationPage = () => {
   const navigate = useNavigate();
@@ -29,7 +30,6 @@ const RegistrationPage = () => {
   const handleRegistration = (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log(email, password, userName, themeColor, profilePicture);
     const theme = themeColor;
     dispatch(registerUser({ userData: { userName, password, email, theme}, profilePicture: profilePicture }));
   };
@@ -94,7 +94,7 @@ const RegistrationPage = () => {
       </div>
 
           <div>
-            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{loading ? <SpinnerCircular /> : 'Register'}</button>
           </div>
         </form>
         <p class="mt-10 text-center text-sm text-gray-500 cursor-pointer">
